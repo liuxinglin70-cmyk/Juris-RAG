396308f7607d653dc88ef94c868276cde76b9818
LIU <liuxinglin79@gmail.com>
2025-12-28 21:46:49 +0800

    代码完善

--- src/__init__.py
+++ src/__init__.py
@@ -5,8 +5,7 @@ Juris-RAG 源代码包
 __version__ = "1.0.0"
 __author__ = "Your Name"
 
-from .config import Config
-from .data_processing import DataProcessor
-from .rag_engine import RAGEngine
+from .data_processing import LegalDataProcessor
+from .rag_engine import JurisRAGEngine, get_rag_engine
 
-__all__ = ["Config", "DataProcessor", "RAGEngine"]
+__all__ = ["LegalDataProcessor", "JurisRAGEngine", "get_rag_engine"]
